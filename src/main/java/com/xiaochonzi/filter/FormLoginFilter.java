b43d25eeb01891f8b303e99ae30211427aad8bf4
stone <xiaochonzi@outlook.com>
2017-08-09 23:18:55 +0800

    增加login拦截器

--- src/main/java/com/xiaochonzi/filter/FormLoginFilter.java
+++ src/main/java/com/xiaochonzi/filter/FormLoginFilter.java
@@ -1,9 +1,14 @@
 package com.xiaochonzi.filter;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.web.filter.PathMatchingFilter;
+import org.apache.shiro.web.util.WebUtils;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by stone on 17/8/7.
@@ -14,6 +19,35 @@ public class FormLoginFilter extends PathMatchingFilter{
 
     @Override
     protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
-        return super.onPreHandle(request, response, mappedValue);
+        if(SecurityUtils.getSubject().isAuthenticated()){
+            return true;
+        }
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        if(isLoginRequest(req)){
+            if("post".equalsIgnoreCase(req.getMethod())){
+                boolean loginSuccess = login(req);
+                if(loginSuccess){
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean login(HttpServletRequest req) {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        try{
+            SecurityUtils.getSubject().login(new UsernamePasswordToken(username,password));
+        }catch (Exception e){
+            req.setAttribute("shiroLoginFailure",e.getClass());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isLoginRequest(HttpServletRequest req){
+        return pathsMatch(loginUrl, WebUtils.getPathWithinApplication(req));
     }
 }
--- src/main/java/com/xiaochonzi/shiro/UserRealm.java
+++ src/main/java/com/xiaochonzi/shiro/UserRealm.java
@@ -1,15 +1,12 @@
 package com.xiaochonzi.shiro;
 
 import com.xiaochonzi.entity.User;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.authz.UnauthenticatedException;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by stone on 17/8/7.
@@ -19,7 +16,7 @@ public class UserRealm extends AuthorizingRealm {
 
 
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        User currentUser = (User) super.getAvailablePrincipal(principalCollection);
+        User currentUser = (User) principalCollection.getPrimaryPrincipal();
         SimpleAuthorizationInfo authenticationInfo = new SimpleAuthorizationInfo();
         authenticationInfo.addRole(currentUser.getRole().getName());
         authenticationInfo.addStringPermission(String.valueOf(currentUser.getRole().getPermission()));
@@ -27,7 +24,15 @@ public class UserRealm extends AuthorizingRealm {
     }
 
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo();
+        User currentUser = (User) authenticationToken.getPrincipal();
+        if(currentUser == null){
+            throw new UnknownAccountException();
+        }
+        if(Boolean.TRUE.equals(currentUser.getConfirmed())){
+            throw new UnauthenticatedException();
+        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(currentUser.getId(),
+                currentUser.getPasswordHash(),getName());
         return authenticationInfo;
     }
 }
